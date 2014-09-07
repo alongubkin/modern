464ceef83344f018033b6305bda53b5714889211
Alon Gubkin <alon.gubkin@gmail.com>
2014-09-08 02:00:35 +0300

    JIT support

--- compiler/ModernCompiler/main.cc
+++ compiler/ModernCompiler/main.cc
@@ -1,12 +1,15 @@
-#include "llvm/IR/Constants.h"
+#include "llvm/Analysis/Passes.h"
+#include "llvm/ExecutionEngine/ExecutionEngine.h"
+#include "llvm/IR/DataLayout.h"
 #include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/Instructions.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
-#include "llvm/Support/ManagedStatic.h"
+#include "llvm/IR/Verifier.h"
+#include "llvm/PassManager.h"
 #include "llvm/Support/TargetSelect.h"
-#include "llvm/Support/raw_ostream.h"
+#include "llvm/Transforms/Scalar.h"
+#include "llvm/ExecutionEngine/JIT.h"
 
 #include <string>
 #include <iostream>
@@ -29,7 +32,43 @@ int main(int argc, char *argv[])
 
 	node->Codegen(module, builder, NULL);
 	
+	// create JIT
+
+	std::string ErrStr;
+	llvm::ExecutionEngine *executionEngine =
+		llvm::EngineBuilder(&module).setErrorStr(&ErrStr).create();
+	if (!executionEngine) {
+		fprintf(stderr, "Could not create ExecutionEngine: %s\n", ErrStr.c_str());
+		getchar();
+	}
+
+	// add optimizations
+
+	llvm::FunctionPassManager fpm(&module);
+
+	// Set up the optimizer pipeline.  Start with registering info about how the
+	// target lays out data structures.
+	module.setDataLayout(executionEngine->getDataLayout());
+	fpm.add(new llvm::DataLayoutPass(&module));
+	// Provide basic AliasAnalysis support for GVN.
+	fpm.add(llvm::createBasicAliasAnalysisPass());
+	// Do simple "peephole" optimizations and bit-twiddling optzns.
+	fpm.add(llvm::createInstructionCombiningPass());
+	// Reassociate expressions.
+	fpm.add(llvm::createReassociatePass());
+	// Eliminate Common SubExpressions.
+	fpm.add(llvm::createGVNPass());
+	// Simplify the control flow graph (deleting unreachable blocks, etc).
+	fpm.add(llvm::createCFGSimplificationPass());
+
+	fpm.doInitialization();
+
+	void *mainFunctionPtr = executionEngine->getPointerToFunction(module.getFunction("main"));
+	int(*FP)() = (int(*)())(intptr_t)mainFunctionPtr;
+
 	llvm::outs() << module;
+
+	fprintf(stderr, "\nPROGRAM EVALUATED TO: %d\n", FP());
 
 	getchar();
 
